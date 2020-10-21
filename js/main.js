6811ca5cce6bc78a69fc0c5d9c92a0006cd6e5fd
Mr2nec <vladislav.shpilka@gmail.com>
2020-10-21 18:44:34 +0300

    commit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+let sn = document.querySelector('#SN');
+let mac = document.querySelector('#MAC');
+let select = document.querySelector('#select');
+let btn = document.querySelector('#btn');
+let result = document.querySelector('#result');
+
+btn.addEventListener('click', getValue);
+
+async function changePortal(value, sn, mac) {
+    if (value === 'vivat.tv' || value === 'vivat.live') {
+        fetch(
+            `http://portal2.vivat-tv.com/portal/ajax/switch_portals.php?portal=${value}&mac=${mac}&sn=${sn}`,
+            { mode: 'no-cors' }
+        )
+            .then((res) => console.log(res))
+            .catch((e) => console.log(e));
+    }
+
+    if (value === 'new' || value === 'old') {
+        fetch(
+            `http://beer.vivat-tv.com/portal/adm/vlad/switch2portal.php?mac=${mac}&sn=${sn}&portal=${value}`
+        )
+            .then((res) => console.log(res.text))
+            .catch((e) => console.log(e));
+    }
+}
+
+function getValue() {
+    if (sn.value !== '' && mac.value !== '') {
+        console.log('ok');
+        changePortal(select.value, sn.value, mac.value);
+    } else {
+        console.log('не ok');
+    }
+}
